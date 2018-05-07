dcea99a0e25763c126b8b45c69aaf13bc2e1e3aa
renald-morice <renaldmorice@hotmail.fr>
2018-05-07 00:34:37 -0400

    Cache Control + html split

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+header("Cache-Control: max-age=259200"); //30days (60sec * 60min * 24hours * 3days) 
+
+echo '
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+';
+
+readfile("content/header.html");
+
+echo '
+    </head>
+    <body>
+        <div class="container-fluid">
+';
+
+readfile("content/loading-screen.html");
+readfile("content/nav.html");
+readfile("content/website-header.html");
+readfile("content/section-about.html");
+readfile("content/section-studies.html");
+readfile("content/section-certifications.html");
+readfile("content/section-experiences.html");
+readfile("content/section-skills.html");
+readfile("content/section-projects.html");
+readfile("content/section-hobbies.html");
+readfile("content/section-videos.html");
+readfile("content/footer.html");
+
+echo '
+        </div>
+        <!-- JS  ======================================= -->
+		<script src="js/jquery-3.1.1.min.js"></script>
+        <script src="js/scrollreveal.min.js"></script>
+        <script src="js/typed.min.js"></script>
+        <script defer src="js/fontawesome-all.min.js"></script>
+        <script src="js/portfolio.js"></script>
+    </body>
+</html>
+';
+
+?>
