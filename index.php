011434a60a7b121bf198c34698c2b3d59b25ecad
renald-morice <renaldmorice@hotmail.fr>
2018-12-29 14:50:53 -0500

    headers

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 
-header("Cache-Control: max-age=259200"); //30days (60sec * 60min * 24hours * 3days) 
+header("Cache-Control: max-age=259200"); //30days (60sec * 60min * 24hours * 3days)
+header("Strict-Transport-Security: 31536000");
+header("Content-Security-Policy: default-source none; frame-ancestors none");
+header("X-Frame-Options: DENY");
+header("X-XSS-Protection: 1; mode=block");
+header("X-Content-Type-Options: nosniff");
 
 echo '
 <!DOCTYPE html>
